b7f8e2e2ad13570054cbb74f4fe449e1e6e119c5
physicist2018 <physicist2018@yandex.ru>
2025-08-10 17:25:16 +1000

    Add article and some changes to main

--- cmd/classifier-linear-eq/main.go
+++ cmd/classifier-linear-eq/main.go
@@ -68,7 +68,7 @@ func main() {
 		go func() {
 			defer wg.Done()
 			for i := range workChan {
-				fmt.Printf("Start procesing %d row\n", i)
+				fmt.Printf("Запуск обработки %d строки из %d\n", i, r)
 				// Обработка первого столбца
 				errr.Set(i, 0, flMatrix.At(i, 0))
 				eta_d.Set(i, 0, flMatrix.At(i, 0))
@@ -117,7 +117,7 @@ func main() {
 					gf_u.Set(i, j, finalResult.AtVec(8))
 					gf_s.Set(i, j, finalResult.AtVec(9))
 				}
-				fmt.Printf("Stop procesing %d row\n", i)
+				fmt.Printf("Завершена обработка %d строки из %d\n", i, r)
 			}
 		}()
 	}
@@ -151,8 +151,8 @@ func main() {
 	presenter.SaveDenseToCSV(gf_s, filepath.Join(cfg.InputDir, "gf_s.csv"))
 	presenter.SaveDenseToCSV(errr, filepath.Join(cfg.InputDir, "errr.csv"))
 
-	fmt.Println("All results saved.")
-	fmt.Printf("Tuned values of:\n")
+	fmt.Println("Все результаты сохранены.")
+	fmt.Printf("Уточненные значения:\n")
 	fmt.Printf("Gf_d   : %.3e\n", avgMatrix(gf_d))
 	fmt.Printf("Gf_u   : %.3e\n", avgMatrix(gf_u))
 	fmt.Printf("Gf_s   : %.3e\n", avgMatrix(gf_s))
@@ -177,115 +177,3 @@ func avgMatrix(m *mat.Dense) float64 {
 	}
 	return sum / float64(r*(c-1))
 }
-
-// /*
-// Copyright © 2025 Kpnstantin Shmirko <physicist2018@vk.com>
-// */
-// package main
-
-// import (
-// 	"classifier-go/internal/config"
-// 	"classifier-go/internal/presenter"
-// 	prepare "classifier-go/internal/solver"
-// 	"classifier-go/pkg/readmatrix"
-// 	"log"
-// 	"path/filepath"
-
-// 	"gonum.org/v1/gonum/mat"
-// )
-
-// func main() {
-// 	cfg := config.Parse()
-// 	log.Println("Starting classifier...")
-
-// 	// Load input matrices
-// 	flMatrix, err := readmatrix.ReadMatrix(filepath.Join(cfg.InputDir, "FL_cap.txt"))
-// 	if err != nil {
-// 		log.Fatalf("Ошибка чтения матрицы флуоресценции: %v\n", err)
-// 	}
-
-// 	depMatrix, err := readmatrix.ReadMatrix(filepath.Join(cfg.InputDir, "Dep.txt"))
-// 	if err != nil {
-// 		log.Fatalf("Ошибка чтения матрицы деполяризации: %v\n", err)
-// 	}
-
-// 	Kernels := prepare.PrepareMatricesA(cfg)
-// 	r, c := flMatrix.Dims()
-
-// 	eta_u := mat.NewDense(r, c, nil)
-// 	eta_s := mat.NewDense(r, c, nil)
-// 	eta_d := mat.NewDense(r, c, nil)
-// 	delta_u := mat.NewDense(r, c, nil)
-// 	delta_s := mat.NewDense(r, c, nil)
-// 	delta_d := mat.NewDense(r, c, nil)
-// 	gf_u := mat.NewDense(r, c, nil)
-// 	gf_s := mat.NewDense(r, c, nil)
-// 	gf_d := mat.NewDense(r, c, nil)
-// 	errr := mat.NewDense(r, c, nil)
-
-// 	navg := int(float64(cfg.AvgPercent) * float64(cfg.NumPoints))
-// 	for i := 0; i < r; i++ {
-// 		errr.Set(i, 0, flMatrix.At(i, 0))
-// 		eta_d.Set(i, 0, flMatrix.At(i, 0))
-// 		eta_u.Set(i, 0, flMatrix.At(i, 0))
-// 		eta_s.Set(i, 0, flMatrix.At(i, 0))
-// 		delta_d.Set(i, 0, flMatrix.At(i, 0))
-// 		delta_u.Set(i, 0, flMatrix.At(i, 0))
-// 		delta_s.Set(i, 0, flMatrix.At(i, 0))
-// 		gf_d.Set(i, 0, flMatrix.At(i, 0))
-// 		gf_u.Set(i, 0, flMatrix.At(i, 0))
-// 		gf_s.Set(i, 0, flMatrix.At(i, 0))
-// 		for j := 1; j < c; j++ {
-// 			depMatrix.Set(i, j, depMatrix.At(i, j)/100.0)
-// 			b := prepare.FormBVector(depMatrix.At(i, j), flMatrix.At(i, j))
-// 			solutions := mat.NewDense(cfg.NumPoints, 10, nil)
-// 			for k := range cfg.NumPoints {
-
-// 				tmpA := Kernels[k]
-
-// 				x, F := prepare.SolveWithBounds(tmpA, b, []float64{0, 0, 0}, []float64{1, 1, 1})
-
-// 				solutions.Set(k, 0, F)
-// 				solutions.Set(k, 1, x.AtVec(0))
-// 				solutions.Set(k, 2, x.AtVec(1))
-// 				solutions.Set(k, 3, x.AtVec(2))
-// 				solutions.Set(k, 4, tmpA.At(0, 0))
-// 				solutions.Set(k, 5, tmpA.At(0, 1))
-// 				solutions.Set(k, 6, tmpA.At(0, 2))
-// 				solutions.Set(k, 7, tmpA.At(1, 0))
-// 				solutions.Set(k, 8, tmpA.At(1, 1))
-// 				solutions.Set(k, 9, tmpA.At(1, 2))
-// 			}
-
-// 			prepare.SortRows(solutions, 0)
-// 			finalResult := prepare.AveragePRows(solutions, navg)
-// 			errr.Set(i, j, finalResult.AtVec(0))
-// 			eta_d.Set(i, j, finalResult.AtVec(1))
-// 			eta_u.Set(i, j, finalResult.AtVec(2))
-// 			eta_s.Set(i, j, finalResult.AtVec(3))
-// 			delta_d.Set(i, j, finalResult.AtVec(4))
-// 			delta_u.Set(i, j, finalResult.AtVec(5))
-// 			delta_s.Set(i, j, finalResult.AtVec(6))
-// 			gf_d.Set(i, j, finalResult.AtVec(7))
-// 			gf_u.Set(i, j, finalResult.AtVec(8))
-// 			gf_s.Set(i, j, finalResult.AtVec(9))
-
-// 		}
-// 	}
-// 	presenter.GenerateHeatmap(filepath.Join(cfg.InputDir, "Dep.pdf"), "Depolarization", depMatrix)
-// 	presenter.GenerateHeatmap(filepath.Join(cfg.InputDir, "Eta_u.pdf"), "Urban aerosol fraction", eta_u)
-// 	presenter.GenerateHeatmap(filepath.Join(cfg.InputDir, "Eta_d.pdf"), "Dust aerosol fraction", eta_d)
-// 	presenter.GenerateHeatmap(filepath.Join(cfg.InputDir, "Eta_s.pdf"), "Smoke aerosol fraction", eta_s)
-
-// 	presenter.SaveDenseToCSV(eta_u, filepath.Join(cfg.InputDir, "Eta_u.csv"))
-// 	presenter.SaveDenseToCSV(eta_d, filepath.Join(cfg.InputDir, "Eta_d.csv"))
-// 	presenter.SaveDenseToCSV(eta_s, filepath.Join(cfg.InputDir, "Eta_s.csv"))
-// 	presenter.SaveDenseToCSV(delta_u, filepath.Join(cfg.InputDir, "delta_u.csv"))
-// 	presenter.SaveDenseToCSV(delta_d, filepath.Join(cfg.InputDir, "delta_d.csv"))
-// 	presenter.SaveDenseToCSV(delta_s, filepath.Join(cfg.InputDir, "delta_s.csv"))
-// 	presenter.SaveDenseToCSV(gf_u, filepath.Join(cfg.InputDir, "gf_u.csv"))
-// 	presenter.SaveDenseToCSV(gf_d, filepath.Join(cfg.InputDir, "gf_d.csv"))
-// 	presenter.SaveDenseToCSV(gf_s, filepath.Join(cfg.InputDir, "gf_s.csv"))
-// 	presenter.SaveDenseToCSV(errr, filepath.Join(cfg.InputDir, "errr.csv"))
-
-// }
